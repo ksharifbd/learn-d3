5c7fa079220924695fb0a99bc7ac84d80fdfeddf
ksharifbd <ksharifbd@gmail.com>
2020-08-30 15:44:08 +0600

    rectangle from data

--- src/shapes/Rectangle.jsx
+++ src/shapes/Rectangle.jsx
@@ -1,23 +1,38 @@
 import React, { useRef, useEffect } from "react";
+import * as d3 from "d3";
 import appendSvg from "../util/appendSvg";
 
 const Reactangle = () => {
   const rectAreaElement = useRef(null);
 
   useEffect(() => {
-    const svg = appendSvg(rectAreaElement.current, {
-      height: 400,
-      width: 400,
-    });
+    (async () => {
+      const svg = appendSvg(rectAreaElement.current, {
+        height: 400,
+        width: 400,
+      });
 
-    svg
-      .append("rect")
-      .attr("x", 20)
-      .attr("y", 20)
-      .attr("height", 200)
-      .attr("width", 200)
-      .attr("rx", 8)
-      .attr("fill", "purple");
+      const data = await d3.json(
+        "https://run.mocky.io/v3/54a05b08-9c02-4143-b206-a6f8d149522e",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      );
+
+      let rects = svg.selectAll("rect").data(data);
+
+      rects
+        .enter()
+        .append("rect")
+        .attr("x", (data, index) => index * 50 + 25)
+        .attr("y", 20)
+        .attr("height", ({ height }) => Number(height) / 2)
+        .attr("width", 20)
+        .attr("fill", "grey");
+    })();
   }, []);
 
   return <div ref={rectAreaElement} />;
