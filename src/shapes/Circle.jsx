a70759974bcb14c4fdc58e066c5f4d8e66e8d3c4
ksharifbd <ksharifbd@gmail.com>
2020-08-30 15:29:34 +0600

    fetches data from api

--- src/shapes/Circle.jsx
+++ src/shapes/Circle.jsx
@@ -1,26 +1,37 @@
 import React, { useRef, useEffect } from "react";
+import * as d3 from "d3";
 import appendSvg from "../util/appendSvg";
 
 const Circle = () => {
   const circleAreaElement = useRef(null);
 
   useEffect(() => {
-    const svg = appendSvg(circleAreaElement.current, {
-      height: 400,
-      width: 400,
-    });
+    (async () => {
+      const svg = appendSvg(circleAreaElement.current, {
+        height: 400,
+        width: 400,
+      });
 
-    const data = [25, 20, 10, 12, 15];
+      const data = await d3.json(
+        "https://run.mocky.io/v3/a67e82e6-847e-4e7f-951e-3f7f34815f27",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      );
 
-    let circles = svg.selectAll("circle").data(data);
+      let circles = svg.selectAll("circle").data(data);
 
-    circles
-      .enter()
-      .append("circle")
-      .attr("cx", (data, index) => index * 50 + 25)
-      .attr("cy", 25)
-      .attr("r", (data) => data)
-      .attr("fill", "blue");
+      circles
+        .enter()
+        .append("circle")
+        .attr("cx", (data, index) => index * 50 + 25)
+        .attr("cy", 25)
+        .attr("r", ({ age }) => age * 2)
+        .attr("fill", "blue");
+    })();
   }, []);
 
   return <div ref={circleAreaElement} />;
