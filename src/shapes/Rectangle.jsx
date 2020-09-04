8b3bdb90b2a8dca8879df0166ee4eea271ac3ccb
ksharifbd <ksharifbd@gmail.com>
2020-09-04 21:24:18 +0600

    adds svg group

--- src/shapes/Rectangle.jsx
+++ src/shapes/Rectangle.jsx
@@ -2,15 +2,29 @@ import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 import appendSvg from "../util/appendSvg";
 
+const margin = {
+  top: 10,
+  right: 10,
+  bottom: 100,
+  left: 100,
+};
+
+const height = 400 - (margin.top + margin.bottom);
+const width = 600 - (margin.right + margin.left);
+
 const Reactangle = () => {
   const rectAreaElement = useRef(null);
 
   useEffect(() => {
     (async () => {
       const svg = appendSvg(rectAreaElement.current, {
-        height: 300,
-        width: 300,
+        height: height + margin.top + margin.bottom,
+        width: width + margin.right + margin.left,
       });
+
+      const g = svg
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
       const products = await d3.json(
         "https://5f4d0702eeec51001608e7a7.mockapi.io/api/learn-d3/products",
@@ -22,17 +36,17 @@ const Reactangle = () => {
         }
       );
 
-      let rects = svg.selectAll("rect").data(products);
+      let rects = g.selectAll("rect").data(products);
 
       const yScale = d3
         .scaleLinear()
         .domain([0, d3.max(products, ({ price }) => price)])
-        .range([0, 200]);
+        .range([0, height]);
 
       const xScale = d3
         .scaleBand()
         .domain(products.map(({ productName }) => productName))
-        .range([0, 300])
+        .range([0, width])
         .paddingInner(0.3)
         .paddingOuter(0.2);
 
