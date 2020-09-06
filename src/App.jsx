fffde904a93a5ae416da3a5235d188364cb058be
ksharifbd <ksharifbd@gmail.com>
2020-09-06 19:16:41 +0600

    removes circle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Circle from "./shapes/Circle";
 import Rectangle from "./shapes/Rectangle";
 
 function App() {
   return (
     <div style={{ display: "flex" }}>
-      <Circle />
       <Rectangle />
     </div>
   );
