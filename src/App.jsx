84630a55832d7a9e3e1552da91df2318a2b9495b
ksharifbd <ksharifbd@gmail.com>
2020-09-14 06:05:37 +0600

    removes product price chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ProductPrice from "./charts/bar/ProductPrice";
 import MonthlyExpense from "./charts/bar/MonthlyExpense";
 
 function App() {
   return (
     <div style={{ display: "flex" }}>
-      <ProductPrice />
       <MonthlyExpense />
     </div>
   );
